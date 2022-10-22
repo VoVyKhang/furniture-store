0e5af9b6d5242682da9ac8e5bf3832d800fd779f
VoVyKhang <vo.vykhang24@gmail.com>
2022-10-22 09:38:28 +0700

    React router setup

---
+++ src/pages/index.js
@@ -0,0 +1,19 @@
+import Home from "../pages/HomePage";
+import Products from "../pages/ProductsPage";
+import SingleProduct from "../pages/HomePage";
+import About from "../pages/AboutPage";
+import Cart from "../pages/CartPage";
+import Error from "../pages/ErrorPage";
+import Checkout from "../pages/CheckoutPage";
+import PrivateRoute from "../pages/PrivateRoute";
+
+export {
+  Home,
+  Products,
+  SingleProduct,
+  About,
+  Cart,
+  Error,
+  Checkout,
+  PrivateRoute,
+};
