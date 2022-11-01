6193ac887013bbdcba82ccbec287ad2deea8d840
VoVyKhang <vo.vykhang24@gmail.com>
2022-11-01 15:03:19 +0700

    single product

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,6 +1,6 @@
 import Home from "../pages/HomePage";
 import Products from "../pages/ProductsPage";
-import SingleProduct from "../pages/HomePage";
+import SingleProduct from "../pages/SingleProductPage";
 import About from "../pages/AboutPage";
 import Cart from "../pages/CartPage";
 import Error from "../pages/ErrorPage";
